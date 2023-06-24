97fbcc64086d1037481ae74cdab003264ddd9af1
mrandal <marandal@stu.naperville203.org>
2023-06-24 11:39:50 -0500

    started button functions

---
+++ playground/static/js/schedule.js
@@ -0,0 +1,44 @@
+
+
+
+(async () => {
+    period4Button.addEventListener("click", async () => {
+        
+    })
+    period5Button.addEventListener("click", async () => {
+
+    })
+    period6Button.addEventListener("click", async () => {
+
+    })
+
+    // switch to week view if this is clicked
+    weekViewButton.addEventListener("click", async () => {
+        await weekView();
+    })
+
+    // switch to day view if any of these are clicked
+    monButton.addEventListener("click", async () => {
+
+    })
+    tueButton.addEventListener("click", async () => {
+
+    })
+    wedButton.addEventListener("click", async () => {
+
+    })
+    thuButton.addEventListener("click", async () => {
+    
+    })
+    friButton.addEventListener("click", async () => {
+    
+    })
+
+
+    async function weekView() {
+
+    }
+    async function dayView() {
+    
+    }
+})
