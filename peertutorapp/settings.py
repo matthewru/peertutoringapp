c094f0db5b3f117ae26c4bf74c7a7415bc401cf2
mrandal <marandal@stu.naperville203.org>
2023-06-23 20:05:18 -0500

    Revert "getting statics css file to show up"
    This reverts commit 4465f7d92428b9c012a80aa30378cdf14b33a34f.

--- peertutorapp/settings.py
+++ peertutorapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,12 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR,'peertutoringapp/staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
