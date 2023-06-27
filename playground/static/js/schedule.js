c87f10e5b14aa6c962c0df042d7d1b7d06d38c04
mrandal <marandal@stu.naperville203.org>
2023-06-27 00:33:24 -0500

    documentation

--- playground/static/js/schedule.js
+++ playground/static/js/schedule.js
@@ -1,8 +1,7 @@
 (async () => {
 
-    // fetch tutor data and create objects/lists
+    // fetch tutor data and create a list of tutor objects
     const tutorsJson = await fetch("/static/testdata.json").then(res => res.json());
-
     const tutors = tutorsJson.peerTutors;
 
     // create a seperate list of tutor objects for each period
@@ -14,15 +13,16 @@
     console.log(p6Tutors);
 
     // initialize the interface
-    weekViewButton.classList.add("selected");
+    weekViewButton.classList.add("selected"); // default to week view and period 4
     period4Button.classList.add("selected");
     clearInterface();
     await weekView();
 
-    // create variables to keep track of the selected period and day
-    let selectedPeriod = 4; // default to period 4
+    // to keep track of the selected period and day
+    let selectedPeriod = 4; 
     let selectedDay = 0;  // 0 is weekview, 1 is mon, 2 is tue, etc.
 
+    // update the schedule when a new period is selected
     period4Button.addEventListener("click", async () => {
         clearPeriodSelected();
         period4Button.classList.add("selected");
@@ -36,62 +36,67 @@
         period6Button.classList.add("selected");
     })
 
-    // switch to week view if this is clicked
+    // switch to week view
     weekViewButton.addEventListener("click", async () => {
         clearDaySelected();
         weekViewButton.classList.add("selected");
         await weekView();
     })
 
-    // switch to day view if any of these are clicked
+    // switch to day view
     monButton.addEventListener("click", async () => {
         clearDaySelected();
         monButton.classList.add("selected");
-        await dayView();
+        await dayView(1);
     })
     tueButton.addEventListener("click", async () => {
         clearDaySelected();
         tueButton.classList.add("selected");
-        await dayView();
+        await dayView(2);
     })
     wedButton.addEventListener("click", async () => {
         clearDaySelected();
         wedButton.classList.add("selected");
-        await dayView();
+        await dayView(3);
     })
     thuButton.addEventListener("click", async () => {
         clearDaySelected();
         thuButton.classList.add("selected");
-        await dayView();
+        await dayView(4);
     })
     friButton.addEventListener("click", async () => {
         clearDaySelected();
         friButton.classList.add("selected");
-        await dayView();
+        await dayView(5);
     })
 
-    // populate interface with tutor data for every day - only include name
+    // populate week-view div with tutor data for each day - only include name
     async function weekView() {
         clearInterface();
         weekSchedule.style.display = "grid";
     }
 
-    // populate interface with tutor data for the selected day
-    async function dayView() {
+    /**
+     * populate day-schedule div with tutor names and expertie for a specific day
+     * @param {int} day | 1 2 3 4 5
+     * @returns null - this function updates the interface
+     */
+    async function dayView(day) {
         clearInterface();
         daySchedule.style.display = "grid";
     }
 
-    // clear the selected day
+    // clear the selected day every time a new day is selected
     function clearDaySelected(){
+        weekViewButton.classList.remove("selected");
         monButton.classList.remove("selected");
         tueButton.classList.remove("selected");
         wedButton.classList.remove("selected");
         thuButton.classList.remove("selected");
         friButton.classList.remove("selected");
-        weekViewButton.classList.remove("selected");
     }
 
+    // clear the selected period every time a new period is selected
     function clearPeriodSelected(){
         period4Button.classList.remove("selected");
         period5Button.classList.remove("selected");
@@ -99,27 +104,41 @@
     }
 
     /**
-     * This function resets the displayed schedule, called when switching between day and week view
+     * Resets the displayed schedule, called when switching between day and week view
      * @parm null - this function uses local variables
-     * @returns null - this function updates the interface, there is no return value
+     * @returns null - this function updates the interface
      */
     function clearInterface(){
         weekSchedule.style.display = "none";
         daySchedule.style.display = "none";
     }
 
-    // Create a list of tutor objects for each period
-    function sortTutorsByPeriod(tutors){
+    /**
+     * Updates the interface and populates the divs with tutors
+     * -- called every time a filter is updated or the period/day is changed
+     * @param null - this function uses local variables
+     * @returns {Array} filteredTutors - a list of tutor objects that should be displayed
+     * */ 
+    function updateFilters(){
+        let filteredTutors = [];
+        if(selectedPeriod == 4){
+            filteredTutors = p4Tutors;
+        }
+        else if(selectedPeriod == 5){
+            filteredTutors = p5Tutors;
+        }
+        else{filteredTutors = p6Tutors;}
+
 
     }
 
     /**
-     * This function updates the interface and populates the divs with tutors
-     * -- call this every time a filter is changed or the period is changed
-     * @param null - this function uses local variables
-     * @returns null - this function updates the interface, there is no return value
-     * */ 
-    function updateFilters(){
+     * This function decides whether or not a tutor should be displayed based on the filters
+     * -- called only in updateFilters()
+     * @param {Object} tutor - the tutor object to be filtered
+     * @returns {Boolean} - true if the tutor should be displayed, false if not
+     */
+    function classFilter(tutor){
 
     }
 
